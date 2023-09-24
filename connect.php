a5d7178e481e80788bf8cc54690e006f0bf71f40
Mst Rinky Akter <mstrinkya@gmail.com>
2023-09-24 18:05:33 +0600

    insert data

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+$conn=mysqli_connect('localhost','root','','crud');
+if(!$conn){
+    die(mysqli_error($conn));
+    
+}
+
+?>
--- index.php
+++ index.php
@@ -40,7 +40,7 @@
        
       </div>
       <div class="modal-footer">
-      <button type="button" class="btn btn-dark">Submit</button>
+      <button type="button" class="btn btn-dark" onclick='adduser()'>Submit</button>
         <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
       </div>
     </div>
@@ -57,5 +57,28 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <!-- boostrap js  -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script>
+  function adduser(){
+    var nameAdd=$('#completename').val();
+    var emailAdd=$('#completeemail').val();
+    var mobileAdd=$('#completemobile').val();
+    var placeAdd=$('#completeplace').val();
+
+    $.ajax({
+      url:"insert.php",
+      type:'post',
+      data:{
+        nameSend:nameAdd,
+        emailSend:emailAdd,
+        mobileSend:mobileAdd,
+        placeSend:placeAdd
+      },
+      success:function(data,status){
+        console.log(status);
+      }
+
+    });
+  }
+</script>
 </body>
 </html>
